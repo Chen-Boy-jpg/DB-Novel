08e891f76fd13a3faf818fb3524ace7ea3d93a19
DESKTOP-32DSEHK\yyy71 <yyy71243@gmail.com>
2024-11-16 05:34:56 +0800

    fix(login): fix form css problem

--- frontend/pages/login/index.tsx
+++ frontend/pages/login/index.tsx
@@ -1,116 +1,121 @@
-import { Box, Button, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from "@chakra-ui/react";
+
 import React from "react";
 
 const Login = () => {
   return (
-    <Box
-      w={"100vw"}
-      h={"100vh"}
-      display={"flex"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      bgColor={"#F2ECEC"}
-    >
+    <form>
       <Box
-        w={"80%"}
-        h={"80%"}
+        w={"100vw"}
+        h={"100vh"}
         display={"flex"}
-        shadow={"lg"}
-        borderRadius={"2rem"}
-        overflow={"hidden"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        bgColor={"#F2ECEC"}
       >
         <Box
-          w={"70%"}
-          h={"100%"}
-          bgColor={"white"}
+          w={"80%"}
+          maxW={"1540px"}
+          h={"80%"}
+          maxH={"1000px"}
           display={"flex"}
-          justifyContent={"center"}
-          alignItems={"center"}
+          shadow={"lg"}
+          borderRadius={"2rem"}
+          overflow={"hidden"}
         >
-          <Box w={"80%"} h={"80%"}>
-            <form action="">
-              <Box
-                display={"flex"}
-                flexDir={"column"}
-                gap={"5rem"}
-                alignItems={"center"}
-                color={"#3AB19B"}
-              >
-                <Box fontSize={"5rem"} fontWeight={800}>
-                  NOVEL
-                </Box>
-                <Input
-                  placeholder="Account"
-                  w={"50rem"}
-                  variant={"flushed"}
-                  h={"3rem"}
-                  fontSize={"1.5rem"}
-                  bgColor={"#F4F8F7"}
-                  p={"1rem"}
-                  letterSpacing={"5px"}
-                />
-                <Input
-                  type="password"
-                  placeholder="Password"
-                  w={"50rem"}
-                  h={"3rem"}
-                  fontSize={"1.5rem"}
-                  bgColor={"#F4F8F7"}
-                  p={"1rem"}
-                  letterSpacing={"5px"}
-                />
-                <Button
-                  mt={"2rem"}
-                  bgColor={"#3AB19B"}
-                  borderRadius={"10rem"}
-                  w={"20rem"}
-                  h={"5rem"}
-                  fontSize={"1.5rem"}
-                  _hover={{
-                    transform: "scale(1.05)",
-                  }}
-                >
-                  SIGN IN
-                </Button>
-              </Box>
-            </form>
-          </Box>
-        </Box>
-        <Box
-          w={"30%"}
-          h={"100%"}
-          bgColor={"#3AA9AB"}
-          display={"flex"}
-          flexDir={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          gap={"2rem"}
-        >
-          <Box fontSize={"3rem"} color={"white"} fontWeight={800}>
-            Hello ,friend
-          </Box>
-          <Box fontSize={"1rem"} color={"white"} fontWeight={600}>
-            Enter your personal information
-          </Box>
-          <Button
-            variant={"outline"}
-            fontSize={"1.5rem"}
-            color={"white"}
-            h={"4rem"}
-            w={"10rem"}
-            fontWeight={600}
-            borderRadius={"10rem"}
-            _hover={{
-              transform: "scale(1.05)",
-              bgColor: "white",
-              color: "#3AB19B",
-            }}
+          <Box
+            h={"100%"}
+            w={"60%"}
+            display={"flex"}
+            flexDir={"column"}
+            alignItems={"center"}
+            justifyContent={"space-around"}
+            bgColor={"white"}
+            color={"#3AB19B"}
           >
-            SIGN UP
-          </Button>
+            <Box fontSize={"5rem"} h={"20%"} fontWeight={800}>
+              NOVEL
+            </Box>
+            <FormControl w={"80%"}>
+              <Input
+                placeholder="Account"
+                variant={"flushed"}
+                fontSize={"1.5rem"}
+                bgColor={"#F4F8F7"}
+                p={"1rem"}
+                letterSpacing={"5px"}
+              />
+            </FormControl>
+            <FormControl w={"80%"}>
+              <Input
+                type="password"
+                variant={"flushed"}
+                placeholder="Password"
+                fontSize={"1.5rem"}
+                bgColor={"#F4F8F7"}
+                p={"1rem"}
+                letterSpacing={"5px"}
+              />
+            </FormControl>
+
+            <Button
+              color={"white"}
+              bgColor={"#3AB19B"}
+              borderRadius={"10rem"}
+              w={"50%"}
+              h={"5rem"}
+              fontSize={"1.5rem"}
+              _hover={{
+                transform: "scale(1.05)",
+              }}
+            >
+              SIGN IN
+            </Button>
+          </Box>
+
+          <Box
+            w={"40%"}
+            h={"100%"}
+            bgColor={"#3AA9AB"}
+            display={"flex"}
+            flexDir={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={"2rem"}
+          >
+            <Box fontSize={"3rem"} color={"white"} fontWeight={800}>
+              Hello ,friend
+            </Box>
+            <Box fontSize={"1rem"} color={"white"} fontWeight={600}>
+              Enter your personal information
+            </Box>
+            <Button
+              variant={"outline"}
+              fontSize={"1.5rem"}
+              color={"white"}
+              h={"4rem"}
+              w={"10rem"}
+              fontWeight={600}
+              borderRadius={"10rem"}
+              _hover={{
+                transform: "scale(1.05)",
+                bgColor: "white",
+                color: "#3AB19B",
+              }}
+            >
+              SIGN UP
+            </Button>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </form>
   );
 };
 
